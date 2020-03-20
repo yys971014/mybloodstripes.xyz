6de10122ac3c299d04ca1b9e45278b58a81a1a5d
yys971014 <50648574+yys971014@users.noreply.github.com>
2020-03-20 16:07:59 +0800

    2020年3月20日 16:07:44 放弃layUI

--- src/main.js
+++ src/main.js
@@ -2,12 +2,11 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+// 引用jQuery
+import $ from 'jquery';
 // 引入element-UI
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
-// 引入layUI
-import '../static/layui/layui.js'
-import '../static/layui/css/layui.css'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.use(ElementUI); // 引入elementUI
 Vue.config.productionTip = false;
--- src/views/index.vue
+++ src/views/index.vue
@@ -1,15 +1,42 @@
 <template>
     <div class="index">
-        <!-- 这个是首页 -->
-        
-        <!-- <button type="button" class="layui-btn layui-btn-normal">百搭按钮</button> -->
+        <ul class="layui-nav">
+            <li class="layui-nav-item"><a href="">最新活动</a></li>
+            <li class="layui-nav-item layui-this">
+                <a href="javascript:;">产品</a>
+                <dl class="layui-nav-child">
+                    <dd><a href="">选项1</a></dd>
+                    <dd><a href="">选项2</a></dd>
+                    <dd><a href="">选项3</a></dd>
+                </dl>
+            </li>
+            <li class="layui-nav-item"><a href="">大数据</a></li>
+            <li class="layui-nav-item">
+                <a href="javascript:;">解决方案</a>
+                <dl class="layui-nav-child">
+                    <dd><a href="">移动模块</a></dd>
+                    <dd><a href="">后台模版</a></dd>
+                    <dd class="layui-this"><a href="">选中项</a></dd>
+                    <dd><a href="">电商平台</a></dd>
+                </dl>
+            </li>
+            <li class="layui-nav-item"><a href="">社区</a></li>
+        </ul>
+        首页内容
+        <button type="button" class="layui-btn layui-btn-normal">百搭按钮</button>
     </div>
 </template>
 
 <script>
 export default {
-    name:"index",
-    components:{
+    data () {
+        return{
+
+        }
+    },
+    mounted(){
+    },
+    methods:{
 
     }
 }
